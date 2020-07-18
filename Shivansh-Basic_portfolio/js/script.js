7e1bf06d3433aeed0cf18e14252a4d49c2ac2154
shivansh kumar <42717219+shivansh2310@users.noreply.github.com>
2020-07-18 11:28:25 +0530

    Add files via upload

---
+++ Shivansh-Basic_portfolio/js/script.js
@@ -0,0 +1,8 @@
+
+$(function(){
+	$("#about-me").typed({
+		strings: ["Student at IIIT-NR.","Founder of ciphercell and Alpha-maniak.","Open Source Contributer.","Business and Stock-market Enthusiast.","Interested in Pentesting and Red Teaming.","Loves to Develop <&#x2764>."],
+		typeSpeed: 30,
+		loop: true,
+	});
+});
